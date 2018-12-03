f651cc4004c0a92a038207474119f65e37055da8
ungerfall <ungerfall@gmail.com>
2018-12-03 09:02:44 +0400

    Add completed task 1 part 2

--- src/AdventOfCode2018/Program.cs
+++ src/AdventOfCode2018/Program.cs
@@ -15,7 +15,8 @@ namespace AdventOfCode2018
 		{
 			taskSolutionDictionry = new Dictionary<string, ISolution>
 			{
-				["1.1"] = new Solution1p1()
+				["1.1"] = new Solution1p1(),
+				["1.2"] = new Solution1p2()
 			};
 			var helpWords = new string[] {"--help", "-h", "?", "\\?"};
 			if (args == null || args.Length <= 0 || args[0] == null || helpWords.Contains(args[0]))
---
+++ src/AdventOfCode2018/Solution1p2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2018
+{
+	class Solution1p2 : ISolution
+	{
+		public string Solve(IEnumerable<string> task)
+		{
+			var frequencies = new Dictionary<int, object>();
+			frequencies.Add(0, 0);
+			var frequency = 0;
+			var infiniteTask = new InfiniteEnumerable<string>(task);
+			foreach(var changeString in infiniteTask)
+			{
+				var change = int.Parse(changeString);
+				frequency += change;
+				if (frequencies.ContainsKey(frequency))
+				{
+					break;
+				}
+
+				frequencies.Add(frequency, frequency);
+			}
+
+			return frequency.ToString();
+		}
+	}
+}
