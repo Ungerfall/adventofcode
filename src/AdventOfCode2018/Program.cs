05c809ea7d0e7645c14b4f0d44f0cc2539ed979e
ungerfall <ungerfall@gmail.com>
2018-12-04 09:43:52 +0400

    Add [WIP] task 2 part 2

--- src/AdventOfCode2018/Program.cs
+++ src/AdventOfCode2018/Program.cs
@@ -17,7 +17,8 @@ namespace AdventOfCode2018
 			{
 				["1.1"] = new Solution1p1(),
 				["1.2"] = new Solution1p2(),
-				["2.1"] = new Solution2p1()
+				["2.1"] = new Solution2p1(),
+				["2.2"] = new Solution2p2()
 			};
 			var helpWords = new string[] {"--help", "-h", "?", "\\?"};
 			if (args == null || args.Length <= 0 || args[0] == null || helpWords.Contains(args[0]))
---
+++ src/AdventOfCode2018/Solution2p2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2018
+{
+	public class Solution2p2 : ISolution
+	{
+		public string Solve(IEnumerable<string> task)
+		{
+			var sortedSeq = task.OrderBy(x => x).ToArray();
+			var answer = string.Empty;
+			for (int i = 0; i < sortedSeq.Length - 1; i++)
+			{
+				for (int j = i; j < sortedSeq.Length; j++)
+				{
+
+				}
+			}
+
+			return string.Empty;
+		}
+	}
+}
