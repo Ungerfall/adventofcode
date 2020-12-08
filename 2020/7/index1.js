3cc402849b449df70dfd481a8f56b89b44f78dbb
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-12-08 21:14:48 +0400

    Solve 7 part 2

--- 2020/7/index1.js
+++ 2020/7/index1.js
@@ -2,44 +2,43 @@ var fs = require('fs');
 var path = require('path');
 
 const inputFile = path.join(__dirname, "input.txt");
-const rules = //fs.readFileSync(inputFile).toString()
-`shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags.`
-    .split("\n")
-    .filter(s => s.trim().length > 0);
+const rules = fs.readFileSync(inputFile).toString()
+        .split("\n")
+        .filter(s => s.trim().length > 0);
 
 const re = /(.*)bags contain(.*)/;
-const valRe = /(\d+ )?(\w+ \w+) bag/;
+const valRe = /(\d+) (\w+ \w+) bag/;
+const noRe = /\s*no other bag.*/;
 const shiny = "shiny gold";
-const no = "no other";
 const nodes = rules
     .map(x => {
         const r = x.match(re);
         const key = r[1].trim();
-        const children = r[2].split(",").map(c => {
-            const m = c.match(valRe);
-            const amount = +(m[2] == no ? 0 : m[1]);
-            return ({ bag: m[2], amount: amount })
-        });
+        const children = r[2].match(noRe)
+            ? null
+            : r[2].split(",").map(c => {
+                const m = c.match(valRe);
+                return ({ bag: m[2], amount: +m[1] })
+            });
 
         return ({ key, children });
     });
 
 const traverse = (node, state) => {
-    for (let child of node.children) {
-        if (child.bag == no || child.bag == shiny) {
-            state.sum += 1;
-            continue;
-        }
-        node = nodes.find(x => x.key == child.bag);
-        state.sum += child.amount * traverse(node, state);
-        console.log(node, state);
+    if (node.children == null) {
+        return 0;
     }
 
-    return state.sum;
+    let sum = 0;
+    for (let child of node.children) {
+        if (child.bag == shiny)
+            continue;
+
+        node = nodes.find(x => x.key == child.bag);
+        sum += child.amount + child.amount * traverse(node, state);
+    }
+
+    return sum;
 };
 
 const state = { sum: 0 };
