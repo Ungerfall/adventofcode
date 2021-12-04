648d176421654380c397a00fd9c58efe307e0712
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-12-05 00:06:00 +0400

    Solve Day 1 Part 1

--- 2021/AdventOfCode2021_1.cs
+++ 2021/AdventOfCode2021_1.cs
@@ -1,5 +1,8 @@
+using _2021.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
 
 namespace _2021
 {
@@ -13,12 +16,33 @@ namespace _2021
         }
 
         [Function("AdventOfCode2021_1")]
-        [ServiceBusOutput("sbq-adventofcode-output", Connection = "ServiceBusConnection")]
-        public string Run([BlobTrigger("samples-workitems/1.input")] string myBlob, string name)
+        public QueueOutput Run([BlobTrigger("advent-of-code-2021/1.input")] string myBlob)
         {
-            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {myBlob}");
+            _logger.LogInformation("C# Blob trigger function Processed blob");
 
-            return "Hello!";
+            var measurements = myBlob
+                .Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int increases = 0;
+            for (int i = 1; i < measurements.Length; i++)
+            {
+                if (measurements[i] > measurements[i - 1])
+                {
+                    increases++;
+                }
+            }
+
+            _logger.LogInformation($"Number of increases: {increases}");
+
+            return new QueueOutput
+            {
+                Payload = new AdventOfCodeOutput
+                {
+                    Day = "1",
+                    Solution = increases.ToString(),
+                }
+            };
         }
     }
 }
