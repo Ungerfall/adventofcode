9eeeb80c53f9c00fd73a53bb6a8984d226dde721
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-12-05 00:04:34 +0400

    Add output queue models

---
+++ 2021/Models/AdventOfCodeOutput.cs
@@ -0,0 +1,7 @@
+﻿namespace _2021.Models;
+
+public class AdventOfCodeOutput
+{
+    public string Day { get; set; }
+    public string Solution { get; set; }
+}
---
+++ 2021/Models/QueueOutput.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Azure.Functions.Worker;
+
+namespace _2021.Models;
+
+public class QueueOutput
+{
+    [ServiceBusOutput("sbq-adventofcode-output", Connection = "ServiceBusConnection")]
+    public AdventOfCodeOutput Payload { get; set; }
+}
