f102beeac4eee6d1281c1e8d5deecc0358eab527
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-12-05 17:06:24 +0400

    Apply c# namespace feature

--- 2021/AdventOfCode2021_OutputSender.cs
+++ 2021/AdventOfCode2021_OutputSender.cs
@@ -2,25 +2,24 @@ using _2021.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace _2021
+namespace _2021;
+
+public class AdventOfCode2021_OutputSender
 {
-    public class AdventOfCode2021_OutputSender
+    private readonly ILogger _logger;
+
+    public AdventOfCode2021_OutputSender(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
+        _logger = loggerFactory.CreateLogger<AdventOfCode2021_OutputSender>();
+    }
 
-        public AdventOfCode2021_OutputSender(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<AdventOfCode2021_OutputSender>();
-        }
+    [Function("AdventOfCode2021_OutputSender")]
+    [BlobOutput("advent-of-code-2021/{Day}.output")]
+    public string Run(
+        [ServiceBusTrigger("sbq-adventofcode-output", Connection = "ServiceBusConnection")] AdventOfCodeOutput q)
+    {
+        _logger.LogInformation($"C# ServiceBus queue message: {new { q.Day, q.Solution }}");
 
-        [Function("AdventOfCode2021_OutputSender")]
-        [BlobOutput("advent-of-code-2021/{Day}.output")]
-        public string Run(
-            [ServiceBusTrigger("sbq-adventofcode-output", Connection = "ServiceBusConnection")] AdventOfCodeOutput q)
-        {
-            _logger.LogInformation($"C# ServiceBus queue message: {new {q.Day, q.Solution}}");
-
-            return q.Solution;
-        }
+        return q.Solution;
     }
 }
