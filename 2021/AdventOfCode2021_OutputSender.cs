2762c82e2cefbee23d4e81f28bb59052be59f8f1
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-12-05 19:38:05 +0400

    Set up SendGrid output binding

--- 2021/AdventOfCode2021_OutputSender.cs
+++ 2021/AdventOfCode2021_OutputSender.cs
@@ -14,12 +14,15 @@ public class AdventOfCode2021_OutputSender
     }
 
     [Function("AdventOfCode2021_OutputSender")]
-    [BlobOutput("advent-of-code-2021/{Day}.output")]
-    public string Run(
+    public SenderOutput Run(
         [ServiceBusTrigger("sbq-adventofcode-output", Connection = "ServiceBusConnection")] AdventOfCodeOutput q)
     {
         _logger.LogInformation($"C# ServiceBus queue message: {new { q.Day, q.Solution }}");
 
-        return q.Solution;
+        return new SenderOutput
+        {
+            Blob = q.Solution,
+            EmailBody = q.Solution,
+        };
     }
 }
---
+++ 2021/Models/SenderOutput.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Worker;
+
+namespace _2021.Models;
+
+public class SenderOutput
+{
+    [BlobOutput("advent-of-code-2021/{Day}.output")]
+    public string Blob { get; set; }
+
+    [SendGridOutput(ApiKey = "SendGridApiKey")]
+    public string EmailBody { get; set; }
+}
