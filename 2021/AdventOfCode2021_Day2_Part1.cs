2d42a3206b79b712b675d228278bb745bee65c2c
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-12-05 19:57:47 +0400

    Solve Day 2

---
+++ 2021/AdventOfCode2021_Day2_Part1.cs
@@ -0,0 +1,56 @@
+using _2021.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2021;
+
+public class AdventOfCode2021_Day2_Part1
+{
+    private readonly ILogger _logger;
+
+    public AdventOfCode2021_Day2_Part1(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<AdventOfCode2021_Day2_Part1>();
+    }
+
+    [Function("AdventOfCode2021_Day2_Part1")]
+    public QueueOutput Run([BlobTrigger("advent-of-code-2021/2.input")] string blob)
+    {
+        _logger.LogInformation("C# Blob trigger function Processed blob");
+
+        var linePattern = new Regex(@"^(\w+)\s+(\d+)", RegexOptions.Compiled);
+        var instructions = blob
+            .Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x =>
+            {
+                var g = linePattern.Match(x).Groups;
+                return (direction: g[1].Value, len: int.Parse(g[2].Value));
+            })
+            .ToArray();
+        int horizontal = 0;
+        int depth = 0;
+        foreach (var i in instructions)
+        {
+            if (i.direction == "down")
+                depth += i.len;
+            else if (i.direction == "up")
+                depth -= i.len;
+            else
+                horizontal += i.len;
+        }
+
+        _logger.LogInformation($"horizontal: {horizontal}; depth: {depth}");
+
+        return new QueueOutput
+        {
+            Payload = new AdventOfCodeOutput
+            {
+                Day = Days.Day2_Part1,
+                Solution = (horizontal * depth).ToString(),
+            }
+        };
+    }
+}
--- 2021/Days.cs
+++ 2021/Days.cs
@@ -4,4 +4,6 @@ public class Days
 {
     public const string Day1_Part1 = "day1-part1";
     public const string Day1_Part2 = "day1-part2";
+    public const string Day2_Part1 = "day2-part1";
+    public const string Day2_Part2 = "day2-part2";
 }
