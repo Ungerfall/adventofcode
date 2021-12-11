6ec0adab80b9fe14b640dc815d35fbcd2beb8b2e
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-12-11 23:27:13 +0400

    Solve Day 8 Part 1

---
+++ 2021/AdventOfCode2021_Day8_Part1.cs
@@ -0,0 +1,51 @@
+using _2021.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _2021;
+
+public class AdventOfCode2021_Day8_Part1
+{
+    private readonly ILogger _logger;
+
+    public AdventOfCode2021_Day8_Part1(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<AdventOfCode2021_Day8_Part1>();
+    }
+
+    [Function("AdventOfCode2021_Day8_Part1")]
+    public QueueOutput Run([BlobTrigger("advent-of-code-2021/7.input")] string blob)
+    {
+        _logger.LogInformation("C# Blob trigger function Processed blob");
+
+        var input = blob
+            .Split(new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x =>
+            {
+                var tenFour = x.Split('|', StringSplitOptions.RemoveEmptyEntries);
+                Debug.Assert(tenFour.Length == 2);
+                var signal = tenFour[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Debug.Assert(signal.Length == 10);
+                var output = tenFour[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Debug.Assert(output.Length == 4);
+                return new { signal, output };
+            })
+            .ToArray();
+
+        var easyDigits = new[] { 2, 3, 4, 7 };
+        var solution = input.Sum(x => x.output.Where(y => easyDigits.Contains(y.Length)).Count());
+        _logger.LogInformation($"Solution: {solution}");
+
+        return new QueueOutput
+        {
+            Payload = new AdventOfCodeOutput
+            {
+                Day = Days.Day8_Part1,
+                Solution = solution.ToString(),
+            }
+        };
+    }
+}
--- 2021/Days.cs
+++ 2021/Days.cs
@@ -16,4 +16,6 @@ public static class Days
     public const string Day6_Part2 = "day6-part2";
     public const string Day7_Part1 = "day7-part1";
     public const string Day7_Part2 = "day7-part2";
+    public const string Day8_Part1 = "day8-part1";
+    public const string Day8_Part2 = "day8-part2";
 }
