77b32fba9899e4b378f1cbe6a35061bc596c8e95
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-12-15 00:38:50 +0400

    Solve Day 11

---
+++ 2021/AdventOfCode2021_Day11_Part2.cs
@@ -0,0 +1,97 @@
+using _2021.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2021;
+public class AdventOfCode2021_Day11_Part2
+{
+    private readonly ILogger _logger;
+
+    public AdventOfCode2021_Day11_Part2(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<AdventOfCode2021_Day11_Part2>();
+    }
+
+    [Function("AdventOfCode2021_Day11_Part2")]
+    public QueueOutput Run([BlobTrigger("advent-of-code-2021/11.input")] string blob)
+    {
+        _logger.LogInformation("C# Blob trigger function Processed blob");
+
+        int[][] octopuses = blob
+            .Split(new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Select(y => (int)char.GetNumericValue(y)).ToArray())
+            .ToArray() ?? Array.Empty<int[]>();
+
+        var xMax = octopuses.Length;
+        var yMax = octopuses[0].Length;
+        var adjX = new[] { -1, -1, -1, 0, 0, 1, 1, 1 };
+        var adjY = new[] { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+        int step = 0;
+        while (true)
+        {
+            step++;
+            for (int x = 0; x < xMax; x++)
+                for (int y = 0; y < yMax; y++)
+                    octopuses[x][y]++;
+
+            var flashes = new HashSet<(int x, int y)>();
+            bool hasFlashes;
+            do
+            {
+                hasFlashes = false;
+                for (int x = 0; x < xMax; x++)
+                    for (int y = 0; y < yMax; y++)
+                    {
+                        if (octopuses[x][y] > 9)
+                        {
+                            hasFlashes = true;
+                            octopuses[x][y] = 0;
+                            flashes.Add((x, y));
+                            foreach (var i in Enumerable.Range(0, 8))
+                            {
+                                var xx = x + adjX[i];
+                                var yy = y + adjY[i];
+                                if (xx < 0 || xx >= xMax)
+                                    continue;
+                                if (yy < 0 || yy >= yMax)
+                                    continue;
+                                if (flashes.Contains((xx, yy)))
+                                    continue;
+
+                                octopuses[xx][yy]++;
+                            }
+                        }
+                    }
+
+            } while (hasFlashes);
+
+            bool ok = true;
+            for (int x = 0; x < xMax; x++)
+                for (int y = 0; y < yMax; y++)
+                {
+                    if (octopuses[x][y] != 0)
+                        ok = false;
+                }
+
+            if (ok)
+                break;
+        }
+
+        var solution = step;
+
+        _logger.LogInformation($"Solution: {solution}");
+
+        return new QueueOutput
+        {
+            Payload = new AdventOfCodeOutput
+            {
+                Day = Days.Day11_Part2,
+                Solution = solution.ToString(),
+            }
+        };
+    }
+}
